3da1e9d0b4939a3315fc3afe3899bf24e451602a
gleidson fagno <gleidsonfagner838@gmail.com>
2022-11-01 22:05:08 -0300

    gulp md1

---
+++ Gulp/projeto/gulpfile.js
@@ -0,0 +1,11 @@
+// const gulp = requere("gulp")
+
+// function tarefaCSS(cb){
+        
+//         return gulp.src("./vendor/**/*.css")
+
+//             .pipe(gulp.dest("./dist"))
+
+// }
+
+// exports.styles = tarefaCSS()
