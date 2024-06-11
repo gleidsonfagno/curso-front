c3b5d30a353ad155b4e6814c83db3e32b20741a2
gledsonfagno <gleidsonfagner838@gmail.com>
2024-06-11 16:37:40 -0300

    Exercício Módulo 18

--- Gulp/projeto/REDAME.md
+++ Gulp/projeto/REDAME.md
@@ -11,25 +11,30 @@ Kit de ferramentas de automação de tarefas baseada em JavaScript
 - Aplica transformações em arquivos ainda na memória (antes de gravar no disco)
 
 ## Porquê automatizar?
+
 - Melhor organização
 - Evitar repetição de tarefas
 - Mais tempo para focar no mais importante: regras de negócio e experiência do usuário
 
 ## Principais recursos
+
 - Minificação de arquivos
 - Otimização de imagens (para melhor performance)
 - Mescla de arquivos de diferentes origens
 
 ## Transpilação para JS
+
 É o processo de transformação (conversão) de escrita em outras linguagens para o JavaScript. Você poderá escrever em TypeScript ou Babel que o código.
 
 Todo o processo é executado a partir node.js e gulp-cli (linha de comando)
 
 ## Global x Local
+
 - A instalação do Node.js é global
 - Já o Gulp e seus plugins são instalados localmente (em cada projeto)
 
 ## Gerenciadores de pacotes
+
 - Dependências do projeto
 - NPM: Node Package Manager
 - Utiliza o Node.js
@@ -37,12 +42,14 @@ Todo o processo é executado a partir node.js e gulp-cli (linha de comando)
 
 
 ## Instalando o Gulp
+
 - Rodar o Node.js
 - Agora na pasta do projeto, adicionar o npm: npm init -y
 - Instalar o gulp-cli (Command Line Interface) globalmente: npm install gulp-cli -g
 - Criar o gulpfile.js
 
 ## Trabalhando com Gulp
+
 - Baseado em funções: tasks
 - Organização do projeto
 - Otimização de arquivos
@@ -50,7 +57,9 @@ Todo o processo é executado a partir node.js e gulp-cli (linha de comando)
 - Diminuição do tamanho (bytes) de imagens
 
 ## Rodar o projeto
+
 - Executar o comando:
-```
+  
+```bash
 npm install
 ```
--- Gulp/projeto/gulpfile.js
+++ Gulp/projeto/gulpfile.js
@@ -1,11 +1,62 @@
-// const gulp = requere("gulp")
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const cssmin = require("gulp-cssmin");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+const image = require("gulp-imagemin");
 
-// function tarefaCSS(cb){
-        
-//         return gulp.src("./vendor/**/*.css")
+function tarefaCSS(cb) {
+  return gulp
+    .src([
+      "./node_modules/bootstrap/dist/css/bootstrap.css",
+      "./node_modules/@fortawesome/fontawesome-free/css/fontawesome.css",
+      "./vendor/owl/css/owl.css",
+      "./vendor/jquery-ui/jquery-ui.css",
 
-//             .pipe(gulp.dest("./dist"))
+      "./src/css/style.css",
+    ]) //copia os arquivo desta pagina
+    .pipe(concat("styles.css"))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: ".min" })) //libs.min.css
+    .pipe(gulp.dest("./dist/css")); //coloca aqui
+}
 
-// }
+function tarefasJS() {
+  return gulp
+    .src([
+    //   "./node_modules/jquery/dist/jquery.js",
+    //   "./node_modules/bootstrap/dist/js/bootstrap.js",
+      "./vendor/owl/js/owl.js",
+    //   "./vendor/jquery-mask/jquery.mask.min.js",
+    //   "./vendor/jquery-ui/jquery-ui.js",
+      "./vendor/jequery/jquery-3.6.1.js",
+      "./src/js/custom.js",
+    ])
+    .pipe(concat("funciona.js"))
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" })) //libs.min.js
+    .pipe(gulp.dest("./dist/js"));
+}
 
-// exports.styles = tarefaCSS()
+function tarefasImagem() {
+  return gulp
+    .src("./src/images/*")
+    .pipe(
+      image({
+        pngquant: true,
+        optipng: false,
+        zopflipng: true,
+        jpegRecompress: false,
+        mozjpeg: true,
+        gifsicle: true,
+        svgo: true,
+        concurrent: 10,
+        quiet: true,
+      })
+    )
+    .pipe(gulp.dest("./dist/images"));
+}
+
+exports.styles = tarefaCSS;
+exports.scripts = tarefasJS;
+exports.images = tarefasImagem;
